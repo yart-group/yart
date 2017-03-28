f75c305f65dc0147823545449fadff1d5d3b1b77
KungFuJesus3 <great.programmer@op.pl>
2017-03-28 15:12:07 +0200

    complete libarduino v0.1 UML model

--- arduino/build/source/program.cpp
+++ arduino/build/source/program.cpp
@@ -6,7 +6,7 @@ class Program {
       startup_error,
       execution_error,
     };
-  
+
     virtual ExitCode init() = 0;
     virtual ExitCode run() = 0;
 };
@@ -16,3 +16,10 @@ class Programator {
   public:
 };
 
+
+
+_kernel->command("move FORWARD 100");
+"move" "FORWARD" "100"
+"move" 0x716347823;
+
+argc = 3, argv[0] = "move", argv[1] = "FORWADR"
