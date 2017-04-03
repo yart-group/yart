c8746736e3932435e92c5fea1a6a85c96544a87e
KungFuJesus3 <great.programmer@op.pl>
2017-04-03 17:29:45 +0200

    update libarduino uml (now with Debug) & delete stropr

--- code/library/src/src/general/stropr.cpp
+++
@@ -1,8 +0,0 @@
-#include "stropr.h"
-
-bool Stropr::strcmp(const char *a, const char *b){
-  while( *(a++) == *(b++) ); a--; b--;
-  if( !(*a && *b) ) return true;
-  if(*a == *b) return true;
-  return false;
-}
