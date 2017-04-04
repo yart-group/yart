14228944aa1427994ed8b8b9305ecf0fc1c10d54
extraking <rafalbrauner@gmail.com>
2017-04-04 07:47:17 +0200

    change tab and container in *.ui

--- simulator/simulator/build-simulator-Desktop_Qt_5_8_0_GCC_64bit-Debug/ui_mainwindow.h
+++ simulator/simulator/build-simulator-Desktop_Qt_5_8_0_GCC_64bit-Debug/ui_mainwindow.h
@@ -13,7 +13,7 @@
 #include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
-#include <QtWidgets/QFrame>
+#include <QtWidgets/QGraphicsView>
 #include <QtWidgets/QGridLayout>
 #include <QtWidgets/QHeaderView>
 #include <QtWidgets/QMainWindow>
@@ -30,13 +30,15 @@ class Ui_MainWindow
 public:
     QWidget *centralWidget;
     QGridLayout *gridLayout;
-    QTabWidget *upperTabs;
-    QWidget *tab001;
-    QWidget *tab002;
     QTabWidget *lowerTabs;
     QWidget *tab003;
     QWidget *tab004;
-    QFrame *workspace;
+    QTabWidget *upperTabs;
+    QWidget *partsTab;
+    QGridLayout *gridLayout_2;
+    QGraphicsView *parts;
+    QWidget *tab002;
+    QGraphicsView *workspace;
     QMenuBar *menuBar;
     QStatusBar *statusBar;
     QToolBar *toolBar;
@@ -45,34 +47,21 @@ public:
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QStringLiteral("MainWindow"));
-        MainWindow->resize(400, 300);
+        MainWindow->resize(800, 600);
         centralWidget = new QWidget(MainWindow);
         centralWidget->setObjectName(QStringLiteral("centralWidget"));
         gridLayout = new QGridLayout(centralWidget);
         gridLayout->setSpacing(6);
         gridLayout->setContentsMargins(11, 11, 11, 11);
         gridLayout->setObjectName(QStringLiteral("gridLayout"));
-        upperTabs = new QTabWidget(centralWidget);
-        upperTabs->setObjectName(QStringLiteral("upperTabs"));
+        lowerTabs = new QTabWidget(centralWidget);
+        lowerTabs->setObjectName(QStringLiteral("lowerTabs"));
         QSizePolicy sizePolicy(QSizePolicy::Maximum, QSizePolicy::Expanding);
         sizePolicy.setHorizontalStretch(0);
         sizePolicy.setVerticalStretch(0);
-        sizePolicy.setHeightForWidth(upperTabs->sizePolicy().hasHeightForWidth());
-        upperTabs->setSizePolicy(sizePolicy);
-        upperTabs->setMovable(true);
-        tab001 = new QWidget();
-        tab001->setObjectName(QStringLiteral("tab001"));
-        upperTabs->addTab(tab001, QString());
-        tab002 = new QWidget();
-        tab002->setObjectName(QStringLiteral("tab002"));
-        upperTabs->addTab(tab002, QString());
-
-        gridLayout->addWidget(upperTabs, 0, 0, 1, 1);
-
-        lowerTabs = new QTabWidget(centralWidget);
-        lowerTabs->setObjectName(QStringLiteral("lowerTabs"));
         sizePolicy.setHeightForWidth(lowerTabs->sizePolicy().hasHeightForWidth());
         lowerTabs->setSizePolicy(sizePolicy);
+        lowerTabs->setMinimumSize(QSize(200, 0));
         lowerTabs->setMovable(true);
         tab003 = new QWidget();
         tab003->setObjectName(QStringLiteral("tab003"));
@@ -83,17 +72,45 @@ public:
 
         gridLayout->addWidget(lowerTabs, 1, 0, 1, 1);
 
-        workspace = new QFrame(centralWidget);
+        upperTabs = new QTabWidget(centralWidget);
+        upperTabs->setObjectName(QStringLiteral("upperTabs"));
+        sizePolicy.setHeightForWidth(upperTabs->sizePolicy().hasHeightForWidth());
+        upperTabs->setSizePolicy(sizePolicy);
+        upperTabs->setMinimumSize(QSize(200, 0));
+        upperTabs->setMovable(true);
+        partsTab = new QWidget();
+        partsTab->setObjectName(QStringLiteral("partsTab"));
+        gridLayout_2 = new QGridLayout(partsTab);
+        gridLayout_2->setSpacing(6);
+        gridLayout_2->setContentsMargins(11, 11, 11, 11);
+        gridLayout_2->setObjectName(QStringLiteral("gridLayout_2"));
+        parts = new QGraphicsView(partsTab);
+        parts->setObjectName(QStringLiteral("parts"));
+        QSizePolicy sizePolicy1(QSizePolicy::Ignored, QSizePolicy::Ignored);
+        sizePolicy1.setHorizontalStretch(0);
+        sizePolicy1.setVerticalStretch(0);
+        sizePolicy1.setHeightForWidth(parts->sizePolicy().hasHeightForWidth());
+        parts->setSizePolicy(sizePolicy1);
+        parts->setMinimumSize(QSize(0, 0));
+
+        gridLayout_2->addWidget(parts, 0, 0, 1, 1);
+
+        upperTabs->addTab(partsTab, QString());
+        tab002 = new QWidget();
+        tab002->setObjectName(QStringLiteral("tab002"));
+        upperTabs->addTab(tab002, QString());
+
+        gridLayout->addWidget(upperTabs, 0, 0, 1, 1);
+
+        workspace = new QGraphicsView(centralWidget);
         workspace->setObjectName(QStringLiteral("workspace"));
-        workspace->setFrameShape(QFrame::StyledPanel);
-        workspace->setFrameShadow(QFrame::Raised);
 
         gridLayout->addWidget(workspace, 0, 1, 2, 1);
 
         MainWindow->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(MainWindow);
         menuBar->setObjectName(QStringLiteral("menuBar"));
-        menuBar->setGeometry(QRect(0, 0, 400, 19));
+        menuBar->setGeometry(QRect(0, 0, 800, 19));
         MainWindow->setMenuBar(menuBar);
         statusBar = new QStatusBar(MainWindow);
         statusBar->setObjectName(QStringLiteral("statusBar"));
@@ -104,8 +121,8 @@ public:
 
         retranslateUi(MainWindow);
 
-        upperTabs->setCurrentIndex(0);
         lowerTabs->setCurrentIndex(0);
+        upperTabs->setCurrentIndex(0);
 
 
         QMetaObject::connectSlotsByName(MainWindow);
@@ -114,10 +131,10 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", Q_NULLPTR));
-        upperTabs->setTabText(upperTabs->indexOf(tab001), QApplication::translate("MainWindow", "Tab 1", Q_NULLPTR));
-        upperTabs->setTabText(upperTabs->indexOf(tab002), QApplication::translate("MainWindow", "Tab 2", Q_NULLPTR));
         lowerTabs->setTabText(lowerTabs->indexOf(tab003), QApplication::translate("MainWindow", "Tab 3", Q_NULLPTR));
         lowerTabs->setTabText(lowerTabs->indexOf(tab004), QApplication::translate("MainWindow", "Tab 4", Q_NULLPTR));
+        upperTabs->setTabText(upperTabs->indexOf(partsTab), QApplication::translate("MainWindow", "Parts", Q_NULLPTR));
+        upperTabs->setTabText(upperTabs->indexOf(tab002), QApplication::translate("MainWindow", "Tab 2", Q_NULLPTR));
         toolBar->setWindowTitle(QApplication::translate("MainWindow", "toolBar", Q_NULLPTR));
     } // retranslateUi
 
