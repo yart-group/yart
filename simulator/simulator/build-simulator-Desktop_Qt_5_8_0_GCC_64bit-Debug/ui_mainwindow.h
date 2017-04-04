5a1a8470e53672a0e9e8fbbcdf3f44ff17887e2d
extraking <rafalbrauner@gmail.com>
2017-04-04 15:18:16 +0200

    improve basic simulator gui appearance

--- simulator/simulator/build-simulator-Desktop_Qt_5_8_0_GCC_64bit-Debug/ui_mainwindow.h
+++ simulator/simulator/build-simulator-Desktop_Qt_5_8_0_GCC_64bit-Debug/ui_mainwindow.h
@@ -20,6 +20,7 @@
 #include <QtWidgets/QMenu>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QTabWidget>
+#include <QtWidgets/QTreeWidget>
 #include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
 
@@ -34,6 +35,8 @@ public:
     QVBoxLayout *verticalLayout_2;
     QTabWidget *controlTabWidget;
     QWidget *tab_3;
+    QVBoxLayout *verticalLayout;
+    QTreeWidget *treeWidget;
     QWidget *tab_4;
     QWidget *tab_5;
     QTabWidget *logsTabWidget;
@@ -60,7 +63,7 @@ public:
         leftWidget = new QWidget(centralWidget);
         leftWidget->setObjectName(QStringLiteral("leftWidget"));
         leftWidget->setMinimumSize(QSize(210, 0));
-        leftWidget->setMaximumSize(QSize(210, 16777215));
+        leftWidget->setMaximumSize(QSize(350, 16777215));
         verticalLayout_2 = new QVBoxLayout(leftWidget);
         verticalLayout_2->setSpacing(0);
         verticalLayout_2->setContentsMargins(11, 11, 11, 11);
@@ -68,8 +71,40 @@ public:
         verticalLayout_2->setContentsMargins(0, 0, 0, 0);
         controlTabWidget = new QTabWidget(leftWidget);
         controlTabWidget->setObjectName(QStringLiteral("controlTabWidget"));
+        controlTabWidget->setCursor(QCursor(Qt::ArrowCursor));
         tab_3 = new QWidget();
         tab_3->setObjectName(QStringLiteral("tab_3"));
+        verticalLayout = new QVBoxLayout(tab_3);
+        verticalLayout->setSpacing(0);
+        verticalLayout->setContentsMargins(11, 11, 11, 11);
+        verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
+        verticalLayout->setContentsMargins(0, 0, 0, 0);
+        treeWidget = new QTreeWidget(tab_3);
+        QTreeWidgetItem *__qtreewidgetitem = new QTreeWidgetItem(treeWidget);
+        QTreeWidgetItem *__qtreewidgetitem1 = new QTreeWidgetItem(__qtreewidgetitem);
+        QTreeWidgetItem *__qtreewidgetitem2 = new QTreeWidgetItem(__qtreewidgetitem1);
+        new QTreeWidgetItem(__qtreewidgetitem2);
+        QTreeWidgetItem *__qtreewidgetitem3 = new QTreeWidgetItem(__qtreewidgetitem);
+        new QTreeWidgetItem(__qtreewidgetitem3);
+        QTreeWidgetItem *__qtreewidgetitem4 = new QTreeWidgetItem(__qtreewidgetitem);
+        new QTreeWidgetItem(__qtreewidgetitem4);
+        new QTreeWidgetItem(treeWidget);
+        QTreeWidgetItem *__qtreewidgetitem5 = new QTreeWidgetItem(treeWidget);
+        QTreeWidgetItem *__qtreewidgetitem6 = new QTreeWidgetItem(__qtreewidgetitem5);
+        QTreeWidgetItem *__qtreewidgetitem7 = new QTreeWidgetItem(__qtreewidgetitem6);
+        new QTreeWidgetItem(__qtreewidgetitem7);
+        new QTreeWidgetItem(__qtreewidgetitem7);
+        new QTreeWidgetItem(__qtreewidgetitem7);
+        new QTreeWidgetItem(__qtreewidgetitem5);
+        new QTreeWidgetItem(treeWidget);
+        QTreeWidgetItem *__qtreewidgetitem8 = new QTreeWidgetItem(treeWidget);
+        new QTreeWidgetItem(__qtreewidgetitem8);
+        new QTreeWidgetItem(__qtreewidgetitem8);
+        new QTreeWidgetItem(__qtreewidgetitem8);
+        treeWidget->setObjectName(QStringLiteral("treeWidget"));
+
+        verticalLayout->addWidget(treeWidget);
+
         controlTabWidget->addTab(tab_3, QString());
         tab_4 = new QWidget();
         tab_4->setObjectName(QStringLiteral("tab_4"));
@@ -90,6 +125,7 @@ public:
         tab_2 = new QWidget();
         tab_2->setObjectName(QStringLiteral("tab_2"));
         logsTabWidget->addTab(tab_2, QString());
+        controlTabWidget->raise();
 
         verticalLayout_2->addWidget(logsTabWidget);
 
@@ -123,7 +159,7 @@ public:
 
         retranslateUi(MainWindow);
 
-        controlTabWidget->setCurrentIndex(2);
+        controlTabWidget->setCurrentIndex(0);
 
 
         QMetaObject::connectSlotsByName(MainWindow);
@@ -132,6 +168,59 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", Q_NULLPTR));
+        QTreeWidgetItem *___qtreewidgetitem = treeWidget->headerItem();
+        ___qtreewidgetitem->setText(1, QApplication::translate("MainWindow", "name", Q_NULLPTR));
+        ___qtreewidgetitem->setText(0, QApplication::translate("MainWindow", "type", Q_NULLPTR));
+
+        const bool __sortingEnabled = treeWidget->isSortingEnabled();
+        treeWidget->setSortingEnabled(false);
+        QTreeWidgetItem *___qtreewidgetitem1 = treeWidget->topLevelItem(0);
+        ___qtreewidgetitem1->setText(0, QApplication::translate("MainWindow", "Gadgets", Q_NULLPTR));
+        QTreeWidgetItem *___qtreewidgetitem2 = ___qtreewidgetitem1->child(0);
+        ___qtreewidgetitem2->setText(0, QApplication::translate("MainWindow", "Input", Q_NULLPTR));
+        QTreeWidgetItem *___qtreewidgetitem3 = ___qtreewidgetitem2->child(0);
+        ___qtreewidgetitem3->setText(0, QApplication::translate("MainWindow", "Sensor", Q_NULLPTR));
+        QTreeWidgetItem *___qtreewidgetitem4 = ___qtreewidgetitem3->child(0);
+        ___qtreewidgetitem4->setText(0, QApplication::translate("MainWindow", "InfraredSensor", Q_NULLPTR));
+        QTreeWidgetItem *___qtreewidgetitem5 = ___qtreewidgetitem1->child(1);
+        ___qtreewidgetitem5->setText(0, QApplication::translate("MainWindow", "Output", Q_NULLPTR));
+        QTreeWidgetItem *___qtreewidgetitem6 = ___qtreewidgetitem5->child(0);
+        ___qtreewidgetitem6->setText(0, QApplication::translate("MainWindow", "Motor", Q_NULLPTR));
+        QTreeWidgetItem *___qtreewidgetitem7 = ___qtreewidgetitem1->child(2);
+        ___qtreewidgetitem7->setText(0, QApplication::translate("MainWindow", "IO", Q_NULLPTR));
+        QTreeWidgetItem *___qtreewidgetitem8 = ___qtreewidgetitem7->child(0);
+        ___qtreewidgetitem8->setText(0, QApplication::translate("MainWindow", "Cable", Q_NULLPTR));
+        QTreeWidgetItem *___qtreewidgetitem9 = treeWidget->topLevelItem(2);
+        ___qtreewidgetitem9->setText(0, QApplication::translate("MainWindow", "Devices", Q_NULLPTR));
+        QTreeWidgetItem *___qtreewidgetitem10 = ___qtreewidgetitem9->child(0);
+        ___qtreewidgetitem10->setText(0, QApplication::translate("MainWindow", "Robot", Q_NULLPTR));
+        QTreeWidgetItem *___qtreewidgetitem11 = ___qtreewidgetitem10->child(0);
+        ___qtreewidgetitem11->setText(1, QApplication::translate("MainWindow", "\"goofy\"", Q_NULLPTR));
+        ___qtreewidgetitem11->setText(0, QApplication::translate("MainWindow", "Goofy", Q_NULLPTR));
+        QTreeWidgetItem *___qtreewidgetitem12 = ___qtreewidgetitem11->child(0);
+        ___qtreewidgetitem12->setText(1, QApplication::translate("MainWindow", "\"left motor\"", Q_NULLPTR));
+        ___qtreewidgetitem12->setText(0, QApplication::translate("MainWindow", "Motor", Q_NULLPTR));
+        QTreeWidgetItem *___qtreewidgetitem13 = ___qtreewidgetitem11->child(1);
+        ___qtreewidgetitem13->setText(1, QApplication::translate("MainWindow", "\"right motor\"", Q_NULLPTR));
+        ___qtreewidgetitem13->setText(0, QApplication::translate("MainWindow", "Motor", Q_NULLPTR));
+        QTreeWidgetItem *___qtreewidgetitem14 = ___qtreewidgetitem11->child(2);
+        ___qtreewidgetitem14->setText(1, QApplication::translate("MainWindow", "\"ir sensor\"", Q_NULLPTR));
+        ___qtreewidgetitem14->setText(0, QApplication::translate("MainWindow", "Sensor", Q_NULLPTR));
+        QTreeWidgetItem *___qtreewidgetitem15 = ___qtreewidgetitem9->child(1);
+        ___qtreewidgetitem15->setText(0, QApplication::translate("MainWindow", "Motherboard", Q_NULLPTR));
+        QTreeWidgetItem *___qtreewidgetitem16 = treeWidget->topLevelItem(4);
+        ___qtreewidgetitem16->setText(0, QApplication::translate("MainWindow", "Objects", Q_NULLPTR));
+        QTreeWidgetItem *___qtreewidgetitem17 = ___qtreewidgetitem16->child(0);
+        ___qtreewidgetitem17->setText(1, QApplication::translate("MainWindow", "static wall", Q_NULLPTR));
+        ___qtreewidgetitem17->setText(0, QApplication::translate("MainWindow", "Obstacle", Q_NULLPTR));
+        QTreeWidgetItem *___qtreewidgetitem18 = ___qtreewidgetitem16->child(1);
+        ___qtreewidgetitem18->setText(1, QApplication::translate("MainWindow", "moving wall", Q_NULLPTR));
+        ___qtreewidgetitem18->setText(0, QApplication::translate("MainWindow", "Obstacle", Q_NULLPTR));
+        QTreeWidgetItem *___qtreewidgetitem19 = ___qtreewidgetitem16->child(2);
+        ___qtreewidgetitem19->setText(1, QApplication::translate("MainWindow", "goofy", Q_NULLPTR));
+        ___qtreewidgetitem19->setText(0, QApplication::translate("MainWindow", "Goofy", Q_NULLPTR));
+        treeWidget->setSortingEnabled(__sortingEnabled);
+
         controlTabWidget->setTabText(controlTabWidget->indexOf(tab_3), QApplication::translate("MainWindow", "settings", Q_NULLPTR));
         controlTabWidget->setTabText(controlTabWidget->indexOf(tab_4), QApplication::translate("MainWindow", "runtime", Q_NULLPTR));
         controlTabWidget->setTabText(controlTabWidget->indexOf(tab_5), QApplication::translate("MainWindow", "map", Q_NULLPTR));
